f5c1022347dabe64867e9b901f921e51fd28615e
enkw <iankwolfe@gmail.com>
2024-04-04 22:00:01 -0400

    Started work on model index

--- models/index.js
+++ models/index.js
@@ -5,12 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: '', // ???????????????
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: '', // ADD FOREIGN KEY ONCE WE FIGURE OUT WHERE THE ASSOCIATION NEEDS TO BE
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: 'ProductTag'});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'ProductTag'});
 
 module.exports = {
   Product,
